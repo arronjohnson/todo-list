b4fdd41e18aa9caa712e7179c305d5fc53144910
Arron Johnson <51274438+arronjohnson@users.noreply.github.com>
2023-07-31 13:00:27 +0100

    Add Project class
    Creates a new object containing a name. Private fields store the
    project's ID, and a map of tasks.
    
    Methods allow the map to be retrieved and manipulated. Additionally,
    the map can be converted to a sorted array. This is used for display
    purposes.
    
    Unique IDs are generated using the uniqid package.

---
+++ src/modules/project.js
@@ -0,0 +1,39 @@
+import generateId from 'uniqid';
+import sortTaskArray from './sort.js';
+
+export default class Project {
+  #id = generateId();
+  #tasks = new Map();
+
+  constructor(name) {
+    this.name = name;
+  }
+
+  getId() {
+    return this.#id;
+  }
+
+  getTasks() {
+    return this.#tasks;
+  }
+
+  // array conversion necessary to allow sorting
+  getTasksAsArray() {
+    return [...this.getTasks().values()];
+  }
+
+  getSortedTasks() {
+    // converting back to a map is unnecessary as we'll just iterate over this to display entries
+    return sortTaskArray(this.getTasksAsArray());
+  }
+
+  addTask(task) {
+    task.setProject(this);
+    this.#tasks.set(task.getId(), task);
+  }
+
+  // id is used here as this is tied to each delete button using data attributes
+  removeTaskById(taskId) {
+    this.#tasks.delete(taskId);
+  }
+}
