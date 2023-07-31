9660dcd54a94e4ab337a00eb88c5957422625010
Arron Johnson <51274438+arronjohnson@users.noreply.github.com>
2023-07-31 13:22:41 +0100

    Add TodoList class
    Creates a new object containing a private field storing an array of
    projects.
    
    Methods allow projects to be added, removed, and retrieved.
    
    Additionally, an array of all tasks across all projects can be
    retrieved in both sorted and unsorted form. This is used for the 'All'
    view.

---
+++ src/modules/todo-list.js
@@ -0,0 +1,31 @@
+import sortTaskArray from './sort.js';
+
+class TodoList {
+  // map isn't used as project quantity is likely minimal, so reduced lookup time is negligible
+  #projects = [];
+
+  addProject(project) {
+    this.#projects.push(project);
+  }
+
+  // ids are used to allow easier DOM manipulation
+  removeProjectById(projectId) {
+    this.#projects = this.#projects.filter((project) => project.getId() !== projectId);
+  }
+
+  getProjectById(projectId) {
+    return this.#projects.find((project) => project.getId() === projectId);
+  }
+
+  // this enables an 'All' view, which displays all tasks from every project on a single page
+  getAllTasks() {
+    return this.#projects.reduce((tasks, project) => tasks.concat(project.getTasksAsArray()), []);
+  }
+
+  getAllTasksSorted() {
+    return sortTaskArray(this.getAllTasks());
+  }
+}
+
+// only a single instance is ever needed, so that is created and exported
+export default new TodoList();
