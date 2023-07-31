cdf5ba4d7ab98b622cd1810777cee9fe571e55b6
Arron Johnson <51274438+arronjohnson@users.noreply.github.com>
2023-07-31 12:46:52 +0100

    Add Task class
    Creates a new object containing the title, description, due date,
    and priority of a user's task.
    
    Private fields store the task's ID, and a project name and ID. The
    latter are set when the task is added to a specific project, and should
    make it easier to find the right project when manipulating the DOM.
    
    Unique IDs are generated using the uniqid package.

---
+++ src/modules/task.js
@@ -0,0 +1,32 @@
+import generateId from 'uniqid';
+
+export default class Task {
+  #id = generateId();
+  #projectId;
+  #projectName;
+
+  constructor(title, desc, dueDate, priority) {
+    this.title = title;
+    this.desc = desc;
+    this.dueDate = dueDate;
+    this.priority = priority;
+  }
+
+  getId() {
+    return this.#id;
+  }
+
+  // this makes DOM manipulation easier later, as we can easily find the correct project by its ID
+  setProject(project) {
+    this.#projectId = project.getId();
+    this.#projectName = project.name;
+  }
+
+  getProjectId() {
+    return this.#projectId;
+  }
+
+  getProjectName() {
+    return this.#projectName;
+  }
+}
