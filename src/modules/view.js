552e204599cfe3f6a81f2692020ddb38cfdedd0d
Arron Johnson <51274438+arronjohnson@users.noreply.github.com>
2023-08-01 15:34:43 +0100

    Switch to using BEM methodology for class names
    All HTML classes renamed to follow the BEM methodology. CSS and JS have
    both been updated where necessary.

--- src/modules/view.js
+++ src/modules/view.js
@@ -6,15 +6,17 @@ export default class View {
 
   static createProjectItem(project) {
     const button = document.createElement('button');
+    button.className = 'sidebar__btn btn';
     button.textContent = project.name;
     return button;
   }
 
   static renderProjects() {
-    View.resetContainer('.project-container');
+    View.resetElement('js-projects-container');
 
-    const container = document.querySelector('.project-container');
+    const container = document.getElementById('js-projects-container');
     const allBtn = document.createElement('button');
+    allBtn.className = 'sidebar__btn btn';
     allBtn.textContent = 'All';
     container.appendChild(allBtn);
 
@@ -30,18 +32,19 @@ export default class View {
     article.dataset.projectId = task.getProjectId();
 
     const h3 = document.createElement('h3');
+    h3.className = 'task-card__heading';
     h3.textContent = task.title;
 
     const due = document.createElement('p');
-    due.classList = 'due';
+    due.className = 'task-card__due-date';
     due.textContent = `Due ${getRemainingTime(task.dueDate, View.currentDate, { unit: 'day' })}`;
 
     const desc = document.createElement('p');
-    desc.classList = 'desc';
+    desc.className = 'task-card__desc';
     desc.textContent = task.desc;
 
     const deleteBtn = document.createElement('button');
-    deleteBtn.classList = 'delete';
+    deleteBtn.className = 'task-card__btn btn btn--red';
     deleteBtn.textContent = 'Delete';
     deleteBtn.addEventListener('click', () => {
       TodoList.getProjectById(article.dataset.projectId).removeTaskById(article.dataset.id);
@@ -56,15 +59,33 @@ export default class View {
   }
 
   static renderTasks() {
-    View.resetContainer('.task-container');
+    View.resetElement('js-tasks-container');
 
-    const container = document.querySelector('.task-container');
+    const container = document.getElementById('js-tasks-container');
     const allTasks = TodoList.getAllTasksSorted();
     allTasks.forEach((task) => container.appendChild(View.createTaskCard(task)));
   }
 
-  static resetContainer(className) {
-    const container = document.querySelector(className);
-    container.innerHTML = '';
+  static resetElement(elementId) {
+    const element = document.getElementById(elementId);
+    element.innerHTML = '';
+  }
+
+  static registerEventHandlers() {
+    const addProjectBtn = document.getElementById('js-add-project-btn');
+    addProjectBtn.addEventListener('click', () => View.addNewProject());
+
+    const addTaskBtn = document.getElementById('js-add-task-btn');
+    addTaskBtn.addEventListener('click', () => View.addNewTask());
+  }
+
+  static addNewProject() {
+    const dialog = document.getElementById('js-add-project-dialog');
+    dialog.showModal();
+  }
+
+  static addNewTask() {
+    const dialog = document.getElementById('js-add-task-dialog');
+    dialog.showModal();
   }
 }
