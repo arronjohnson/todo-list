3c52d7cf1892133f69eedfb9d8105cc734c70e2e
Arron Johnson <51274438+arronjohnson@users.noreply.github.com>
2023-08-02 13:33:19 +0100

    Remove debug code from index.js
    This is no longer necessary, instead we simply call Storage.load to
    restore the TodoList from local storage.

--- src/index.js
+++ src/index.js
@@ -1,36 +1,5 @@
 import 'normalize.css';
 import './style.scss';
-import TodoList from './modules/todo-list.js';
-import Project from './modules/project.js';
-import Task from './modules/task.js';
-import View from './modules/view.js';
+import Storage from './modules/storage.js';
 
-// TESTING CODE
-const defaultProject = TodoList.getProjectById(TodoList.getDefaultProjectId());
-const defaultProjectTasks = [
-  new Task('Default #1', '', '2024/01/01', 1, ''),
-  new Task('Default #2', '', '2024/05/01', 3, ''),
-  new Task('Default #3', '', '2024/01/27', 1, ''),
-  new Task('Default #4', '', '2024/01/11', 1, ''),
-  new Task('Default #5', '', '2023/12/01', 2, ''),
-];
-
-for (const todo of defaultProjectTasks) {
-  defaultProject.addTask(todo);
-}
-
-const projectTwo = new Project('Testing');
-const projectTwoTasks = [
-  new Task('Testing #1', '', '2023/01/01', 3, ''),
-  new Task('Testing #2', '', '2023/01/01', 2, ''),
-  new Task('Testing #3', '', '2023/07/31', 1, ''),
-];
-
-for (const todo of projectTwoTasks) {
-  projectTwo.addTask(todo);
-}
-
-TodoList.addProject(projectTwo);
-
-window.TodoList = TodoList;
-View.init();
+Storage.load();
